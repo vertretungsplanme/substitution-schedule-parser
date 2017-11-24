8b440492ecc1f4d2e26022ca204949a6c0a15ee5
johan12345 <johan.forstner@gmail.com>
2017-11-24 14:04:42 +0100

    Add Ical parser for Eichendorff Gymnasium Koblenz

---
+++ parser/src/main/java/me/vertretungsplan/additionalinfo/eichendorffgymkoblenz/EichendorffGymKoblenzIcalParser.java
@@ -0,0 +1,18 @@
+/*
+ * substitution-schedule-parser - Java library for parsing schools' substitution schedules
+ * Copyright (c) 2017 Johan v. Forstner
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package me.vertretungsplan.additionalinfo.eichendorffgymkoblenz;
+
+import me.vertretungsplan.additionalinfo.BaseIcalParser;
+
+public class EichendorffGymKoblenzIcalParser extends BaseIcalParser {
+    @Override
+    protected String getIcalUrl() {
+        return "https://calendar.google.com/calendar/ical/eichendorff.koblenz@googlemail.com/public/basic.ics";
+    }
+}
