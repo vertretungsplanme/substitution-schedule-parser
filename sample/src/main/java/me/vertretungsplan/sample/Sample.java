3a698ba6e14dab5b0c1dd1ccb227275721519990
johan12345 <johan.forstner@gmail.com>
2018-07-10 22:42:52 +0200

    Release 1.0.0-beta270

--- sample/src/main/java/me/vertretungsplan/sample/Sample.java
+++ sample/src/main/java/me/vertretungsplan/sample/Sample.java
@@ -6,6 +6,7 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+
 package me.vertretungsplan.sample;
 
 import me.vertretungsplan.ParserUtil;
