c5898d99a0112f20f67a16924aa251f6c6ba7418
johan12345 <johan.forstner@gmail.com>
2019-06-13 17:26:14 +0200

    set user agent

--- parser/src/main/java/me/vertretungsplan/parser/BaseParser.java
+++ parser/src/main/java/me/vertretungsplan/parser/BaseParser.java
@@ -96,6 +96,8 @@ public abstract class BaseParser implements SubstitutionScheduleParser {
                     .setRedirectStrategy(new LaxRedirectStrategy())
                     .setDefaultRequestConfig(RequestConfig.custom()
                             .setCookieSpec(CookieSpecs.STANDARD).build())
+                    .setUserAgent(
+                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
                     .build();
             this.executor = Executor.newInstance(httpclient).use(cookieStore);
         } catch (GeneralSecurityException | JSONException | IOException e) {
