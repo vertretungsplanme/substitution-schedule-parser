51e4dad3b61b0b121d6591e6fb8651bae9ba738a
johan12345 <johan.forstner@gmail.com>
2020-01-06 21:30:19 +0100

    Add additionalInfoParsers for BLS Schleswig

---
+++ parser/src/main/java/me/vertretungsplan/additionalinfo/blsschleswig/lsschleswig/BlsSchleswigRSSParser.java
@@ -0,0 +1,17 @@
+/*
+ * substitution-schedule-parser - Java library for parsing schools' substitution schedules
+ * Copyright (c) 2017 Johan v. Forstner
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package me.vertretungsplan.additionalinfo.blsschleswig.lsschleswig;
+
+import me.vertretungsplan.additionalinfo.BaseRSSFeedParser;
+
+public class BlsSchleswigRSSParser extends BaseRSSFeedParser {
+    @Override protected String getRSSUrl() {
+        return "http://bruno-lorenzen-schule.de/feed";
+    }
+}
